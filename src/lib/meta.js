60363a83a14b55722241a14137e0a3320dda9684
verdulife <verdu@live.com>
2023-01-27 23:11:50 +0100

    home start

---
+++ src/lib/meta.js
@@ -0,0 +1,170 @@
+const BASE_URL = "https://www.facturasgratis.ml";
+const SUFIX = " | Facturasgratis";
+
+export const home = {
+  url: BASE_URL,
+  title: "Facturasgratis | Herramientas de gestión",
+  description: "Herramientas online gratuitas para crear, modificar y guardar facturas, presupuestos, albaranes, clientes, proveedores y productos/servicios.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/mobile.png",
+};
+
+export const facturas = {
+  url: BASE_URL + "/facturas",
+  title: "Facturas" + SUFIX,
+  description: "Crea y/o modifica facturas. Envíalas por correo electrónico y/o guárdalas en PDF.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/facturas.jpg",
+};
+
+export const nueva_factura = {
+  url: BASE_URL + "/facturas/nueva",
+  title: "Crear factura" + SUFIX,
+  description: "Crea y/o modifica facturas. Envíalas por correo electrónico y/o guárdalas en PDF.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/facturas.jpg",
+};
+
+export const presupuestos = {
+  url: BASE_URL + "/presupuestos",
+  title: "Presupuestos" + SUFIX,
+  description: "Crea y/o modifica presupuestos. Envíalos por correo electrónico y/o guárdalos en PDF.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/presupuestos",
+};
+
+export const nueva_presupuesto = {
+  url: BASE_URL + "/presupuestos/nueva",
+  title: "Crear presupuesto" + SUFIX,
+  description: "Herramientas online gratuitas para crear, modificar y guardar facturas, presupuestos, albaranes, clientes, proveedores y productos/servicios.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/presupuestos.jpg",
+};
+
+export const albaranes = {
+  url: BASE_URL + "/albaranes",
+  title: "Albaranes" + SUFIX,
+  description: "Crea y/o modifica albaranes. Envíalos por correo electrónico y/o guárdalos en PDF.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/albaranes.jpg",
+};
+
+export const proformas = {
+  url: BASE_URL + "/proformas",
+  title: "Proformas" + SUFIX,
+  description: "Crea y/o modifica proformas. Envíalas por correo electrónico y/o guárdalas en PDF.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/facturas.jpg",
+};
+
+export const nueva_proforma = {
+  url: BASE_URL + "/proformas/nueva",
+  title: "Crear proforma" + SUFIX,
+  description: "Crea y/o modifica proformas. Envíalas por correo electrónico y/o guárdalas en PDF.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/facturas.jpg",
+};
+
+export const clientes = {
+  url: BASE_URL + "/clientes",
+  title: "Clientes" + SUFIX,
+  description: "Crea una lista de clientes, así podrás usarlos en tus facturas, presupuestos y/o albaranes.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/clients.jpg",
+};
+
+export const nueva_cliente = {
+  url: BASE_URL + "/clientes/nueva",
+  title: "Crear cliente" + SUFIX,
+  description: "Crea una lista de clientes, así podrás usarlos en tus facturas, presupuestos y/o albaranes.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/clientes.jpg",
+};
+
+export const productos_servicios = {
+  url: BASE_URL + "/productos-servicios",
+  title: "Productos y servicios" + SUFIX,
+  description: "Crea una lista de productos/servicios, así podrás usarlos en tus facturas, presupuestos y/o albaranes.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/productos-servicios.jpg",
+};
+
+export const proveedores = {
+  url: BASE_URL + "/proveedores",
+  title: "Proveedores" + SUFIX,
+  description: "Crea una lista de proveedores. Así los tendrás su información de contacto siempre a mano.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/proveedores.jpg",
+};
+
+export const nueva_proveedor = {
+  url: BASE_URL + "/proveedores/nueva",
+  title: "Crear proveedor" + SUFIX,
+  description: "Crea una lista de proveedores. Así los tendrás su información de contacto siempre a mano.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/proveedores.jpg",
+};
+
+export const calculadora = {
+  url: BASE_URL + "/calculadora",
+  title: "Calculadora" + SUFIX,
+  description: "Una calculadora diseñada para calcular de forma automática los impuestos.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/presupuestos.jpg",
+};
+
+export const convertidor = {
+  url: BASE_URL + "/convertidor",
+  title: "Convertidor" + SUFIX,
+  description: "Convierte precios con impuestos a sin impuestos y viceversa.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/productos-servicios.jpg",
+};
+
+export const ajustes = {
+  url: BASE_URL + "/ajustes",
+  title: "Tus datos" + SUFIX,
+  description: "Herramientas online gratuitas para crear, modificar y guardar facturas, presupuestos, albaranes, clientes, proveedores y productos/servicios.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/mobile.png",
+};
+
+export const privacidad = {
+  url: BASE_URL + "/privacidad",
+  title: "Privacidad" + SUFIX,
+  description: "Herramientas online gratuitas para crear, modificar y guardar facturas, presupuestos, albaranes, clientes, proveedores y productos/servicios.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/mobile.png",
+};
+
+export const metro_cuadrado = {
+  url: BASE_URL + "/metro-cuadrado",
+  title: "Calculadora metro cuadrado" + SUFIX,
+  description: "Calcula precios segun el tamaño con la calculadora de metro cuadrado.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/metro.svg",
+};
+
+export const control_gastos = {
+  url: BASE_URL + "/control-gastos",
+  title: "Control de gastos" + SUFIX,
+  description: "Escanea tus tickets y facturas para llevar el control de los gastos.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/gastos.svg",
+};
+
+export const calculadora_descuentos = {
+  url: BASE_URL + "/calculadora-descuentos",
+  title: "Calculadora de descuentos" + SUFIX,
+  description: "Añade uno o varios productos y aplica descuentos individual o globalmente a su valor.",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/descuentos.svg",
+};
+
+export const lector_qr = {
+  url: BASE_URL + "/lector-qr",
+  title: "Lector QR" + SUFIX,
+  description: "Lee, formatea y trabaja con el nuevo estandar de Facturas QR",
+  keywords: "herramientas online, autonomos, gratis, facturas, presupuestos, albaranes, clientes, proveedores, productos, servicios",
+  image: BASE_URL + "/facturas.svg",
+};
---
+++ src/lib/tips.js
@@ -0,0 +1,31 @@
+export const tips = [
+  {
+    title: "No envíes documentos editables",
+    description: `Es habitual hacer facturas en Word o Excel, pero recuerda guárdalas en PDF o dejarlas impresas, así nadie podrá modificar su contenido`,
+  },
+  {
+    title: "Convertir documentos",
+    description: `Si necesitas convertir una factura de Word a PDF, o viceversa, y no
+    sabes como, en <a href="https://www.ilovepdf.com" target="_blank">iLovePDF</a> encontrarás
+    herramientas muy útiles.`,
+  },
+  {
+    title: "Usa la nube",
+    description: `Crea una carpeta en tu Google Drive, Dropbox o similar, y guarda todos
+    tus documentos de facturación, así a final de trimestre podrás
+    compartir la carpeta con tu contable o gestor. Ahorrarás mucho
+    tiempo.`,
+  },
+  {
+    title: "Añade toda la información",
+    description: `Al crear facturas, añade toda la información del cliente y respeta mayúsculas, minúsculas, signos de puntuación y otros. Evitarás muchos problemas.`,
+  },
+  {
+    title: "Notas a pie de página",
+    description: `A pie de tus facturas, presupuestos y albaranes, añade tus condiciones, vencimientos y métodos de pago. Todas las dudas que puedas resolver por escrito, no te molestaran más adelante.`,
+  },
+  {
+    title: "Numeración anual",
+    description: `Usa un prefijo en la numeración de tus facturas, presupuestos y albaranes. Lo más común es añadir los 2 últimos dígitos del año un guion y el número actual. Así evitarás numeraciones altas.`,
+  },
+];
