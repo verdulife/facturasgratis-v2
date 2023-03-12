0f1e8adc0f0222e798d2be4c0f7f54cb5db04342
verdulife <verdu@live.com>
2023-03-12 12:38:28 +0100

    cp autocomplete

--- src/lib/components/facturas/ClientInput.svelte
+++ src/lib/components/facturas/ClientInput.svelte
@@ -1,5 +1,19 @@
 <script>
+	import { es } from '$lib/postal-codes';
 	export let clientsData, client;
+
+	function autocompleteByPostalCode() {
+		const { cp } = client;
+
+		if (!cp || cp.length < 5) return;
+
+		const autocomplete = es.find((code) => code.zipcode === cp);
+
+		if (!autocomplete) return;
+
+		client.city = autocomplete.city;
+		client.country = 'España';
+	}
 </script>
 
 <article class="col wfull">
@@ -21,6 +35,53 @@
 			{/each}
 		</datalist>
 	</label>
+
+	<div class="row wfull">
+		<label class="col wfull" for="legal_id">
+			<small>CIF/NIF</small>
+			<input class="wfull" id="legal_id" type="text" bind:value={client.legal_id} />
+		</label>
+
+		<label class="col wfull" for="contact">
+			<small>Contacto</small>
+			<input class="wfull" id="contact" type="text" bind:value={client.contact} />
+		</label>
+	</div>
+
+	<label class="col wfull" for="address">
+		<small>Calle, número y piso</small>
+		<input class="wfull" id="address" type="text" bind:value={client.address} />
+	</label>
+
+	<label class="col wfull" for="cp">
+		<small>Código postal</small>
+		<input
+			class="wfull"
+			list="postal_codes"
+			id="cp"
+			type="text"
+			bind:value={client.cp}
+			on:input={autocompleteByPostalCode}
+		/>
+
+		<datalist id="postal_codes">
+			{#each es as { zipcode, city }}
+				<option value={zipcode}>{city}</option>
+			{/each}
+		</datalist>
+	</label>
+
+	<div class="row wfull">
+		<label class="col wfull" for="city">
+			<small>Ciudad</small>
+			<input class="wfull" id="city" type="text" bind:value={client.city} />
+		</label>
+
+		<label class="col wfull" for="country">
+			<small>País</small>
+			<input class="wfull" id="country" type="text" bind:value={client.country} />
+		</label>
+	</div>
 </article>
 
 <style lang="postcss">
@@ -56,5 +117,9 @@
 		& input {
 			margin-top: 0.5em;
 		}
+	}
+
+	div {
+		gap: 2em;
 	}
 </style>
